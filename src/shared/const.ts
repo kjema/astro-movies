e49d342a057b52deaecac06c5cec08cbf5239862
Marcus Kjell <marcus.kjell@gmail.com>
2022-04-16 21:59:00 +0200

    dev check for env

--- src/shared/const.ts
+++ src/shared/const.ts
@@ -1,2 +1,5 @@
-export const TMDB_API_KEY =
-  import.meta.env.TMDB_API_KEY ?? process.env.TMDB_API_KEY;
+export const isDevelopment = process.env.NODE_ENV === "development";
+
+export const TMDB_API_KEY = isDevelopment
+  ? import.meta.env.TMDB_API_KEY
+  : process.env.TMDB_API_KEY;
