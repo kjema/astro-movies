c9c19395296ac99c6a4440fb923c4ec32b48f9f5
Marcus Kjell <marcus.kjell@gmail.com>
2022-04-19 22:05:36 +0200

    Fixed imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel";
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
